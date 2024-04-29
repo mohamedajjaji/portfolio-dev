e6ce1840e27d5fd4e11db74036ecaa6f4047cda8
mustapha sahlaoui <anonanonsahlaoui@gmail.com>
2024-04-29 03:31:53 +0100

    this is portfoloi sans project

---
+++ js/style.js
@@ -0,0 +1,26 @@
+var skillPers = document.querySelectorAll(".skill-per");
+
+skillPers.forEach(function(skillPer) {
+  var per = parseFloat(skillPer.getAttribute("per"));
+  skillPer.style.width = per + "%";
+
+  var animatedValue = 0;
+  var startTime = null;
+
+  function animate(timestamp) {
+    if (!startTime) startTime = timestamp;
+    var progress = timestamp - startTime;
+    var stepPercentage = progress / 1000; // Dividing by duration in milliseconds (1000ms = 1s)
+
+    if (stepPercentage < 1) {
+      animatedValue = per * stepPercentage;
+      skillPer.setAttribute("per", Math.floor(animatedValue) + "%");
+      requestAnimationFrame(animate);
+    } else {
+      animatedValue = per;
+      skillPer.setAttribute("per", Math.floor(animatedValue) + "%");
+    }
+  }
+
+  requestAnimationFrame(animate);
+});
